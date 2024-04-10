ebe5c79ecae18ab8a16caf9c58fc26f3a647f5e3
KavyaMacha <124514964+KavyaMacha@users.noreply.github.com>
2024-04-10 19:11:44 +0530

    Initial Commit
    This repository contains a Java application for generating secure passwords. It includes files for the password generation logic, GUI implementation, main entry point, and application management. The project aims to provide users with a convenient tool for creating strong passwords, enhancing digital security practices. Contributions are welcome to improve features and usability.

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        //instantiate the GUI
+        PasswordGeneratorGUI passwordGeneratorGUI=new PasswordGeneratorGUI();
+
+        //make the GUI visible
+        passwordGeneratorGUI.setVisible(true) ;
+        //hi this kavya macha
+        //studing btech
+        //this project is abpu the passwors generation
+
+
+    }
+}
